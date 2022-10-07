4372429a584a5335fb0733ac5e741f2cfa0c4a72
William Skou Heidemann <wihe@itu.dk>
2022-10-07 13:43:56 +0200

    basic item tests

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -23,6 +23,43 @@ public class ProgramTests
         program.Items[0].SellIn.Should().Be(9);
         program.Items[0].Quality.Should().Be(0);
     }
+
+    [Fact]
+    public void QualityCanNotBeLessThan0()
+    {
+        // Arrange
+        var program = new Program();
+        program.Items = new List<Item>();
+        var item = new Item { Name = "Rusty Axe", SellIn = 5, Quality = 0 };
+        program.Items.Add(item);
+
+        // Act
+        program.UpdateQuality();
+
+        // Assert
+        program.Items[0].Name.Should().Be("Rusty Axe");
+        program.Items[0].SellIn.Should().Be(4);
+        program.Items[0].Quality.Should().Be(0);
+    }
+
+    [Fact]
+    public void WhenSellInHasPassedQualityDegradesTwiceAsFast()
+    {
+        // Arrange
+        var program = new Program();
+        program.Items = new List<Item>();
+        var item = new Item { Name = "Blades of Destiny", SellIn = 0, Quality = 4 };
+        program.Items.Add(item);
+
+        // Act
+        program.UpdateQuality();
+
+        // Assert
+        program.Items[0].Name.Should().Be("Blades of Destiny");
+        program.Items[0].SellIn.Should().Be(-1);
+        program.Items[0].Quality.Should().Be(2);
+    }
+
     /*
     [Fact]
     public void Main_when_run_prints_Hello_World()

557fc8b34e5ee26f8d5e81cf2d7937dc88da31ad
Tyyrid <95574559+Tyyrid@users.noreply.github.com>
2022-10-07 14:20:11 +0200

    Implemented tests (#2)
    - Quality
    -- after sell date
    -- never above 50
    -- Brie: stops at 50
    -- Sulfuras: always 80
    
    Co-authored-by: Turið Højgaard <tuho@itu.dk>

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -2,10 +2,87 @@ namespace GildedRose.Tests;
 
 public class ProgramTests
 {
-    [Fact]
+
+   [Fact]
     public void TestTheTruth()
     {
         true.Should().BeTrue();
+    }
+
+    [Fact]
+    public void UpdateQuality_Does_Quality_Degrade() {
+        var app = new Program()
+        {
+            Items = new List<Item> {
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].SellIn.Should().Be(9);
+        app.Items[0].Quality.Should().Be(19);
+
+    }
+
+    [Fact]
+    public void UpdateQuality_Quality_Degrade_After_Sell_Date_Has_Passed() {
+        var app = new Program()
+        {
+            Items = new List<Item> {
+            new Item {Name = "+5 Dexterity Vest", SellIn = -1, Quality = 20}
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].Quality.Should().Be(18);
+    }
+
+    [Fact (Skip = "Not implemented yet")]
+    public void UpdateQuality_Quality_Is_Never_Above_50() {
+        var app = new Program()
+        {
+            Items = new List<Item> {
+            new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 55 },
+            new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 52 },
+            new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 52 }
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].Quality.Should().Be(50);
+        app.Items[1].Quality.Should().Be(50);
+        app.Items[2].Quality.Should().Be(50);
+    }
+
+    [Fact (Skip = "Not implemented yet")]
+    public void UpdateQuality_Quality_Of_Brie_Stops_At_50() {
+        var app = new Program()
+        {
+            Items = new List<Item> {
+            new Item { Name = "Aged Brie", SellIn = 2, Quality = 60 }
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].Quality.Should().Be(50);
+    }
+
+    [Fact (Skip = "Not implemented yet")]
+    public void UpdateQuality_Quality_Of_Sulfuras_Is_Always_80() {
+                var app = new Program()
+        {
+            Items = new List<Item> {
+            new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 70 }
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].Quality.Should().Be(80);
     }
 
     [Fact]
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -5,7 +5,7 @@ namespace GildedRose
 {
     public class Program
     {
-        public IList<Item> Items;
+        public IList<Item>? Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
@@ -42,7 +42,7 @@ namespace GildedRose
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            for (var i = 0; i < Items!.Count; i++)
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
@@ -120,7 +120,7 @@ namespace GildedRose
 
     public class Item
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public int SellIn { get; set; }
 

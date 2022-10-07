f4341a7a6ab1977e8d390f5a4499e59417b25e37
GoodKidJonesy <80992821+GoodKidJonesy@users.noreply.github.com>
2022-10-07 13:47:16 +0200

    Sulfuras and Brie tests (#1)

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -7,4 +7,65 @@ public class ProgramTests
     {
         true.Should().BeTrue();
     }
+
+    [Fact]
+    public void SULFURAS_SHOULD_NOT_CHANGE_QUALITY()
+    {
+        //Arrange
+        var app = new Program()
+        {
+            Items = new List<Item>
+            {
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 79 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 81 }
+            }
+        };
+
+        //Act
+        app.UpdateQuality();
+
+        //Assert
+        app.Items[0].Quality.Should().Be(79);
+        app.Items[1].Quality.Should().Be(80);
+        app.Items[2].Quality.Should().Be(81);
+    }
+
+    [Fact]
+    public void SULFURAS_SHOULD_NOT_UPDATE_SELLIN()
+    {
+        //Arrange
+        var app = new Program()
+        {
+            Items = new List<Item>
+            {
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }
+            }
+        };
+
+        //Act
+        app.UpdateQuality();
+
+        //Assert
+        app.Items[0].SellIn.Should().Be(0);
+    }
+
+    [Fact]
+    public void BRIE_SHOULD_INCREASE_IN_QUALITY()
+    {
+        //Arrange
+        var app = new Program()
+        {
+            Items = new List<Item>
+            {
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
+            }
+        };
+
+        //Act
+        app.UpdateQuality();
+
+        //Assert
+        app.Items[0].Quality.Should().Be(1);
+    }
 }
